62e9bcd7fc3f1e37a70895b0519bac7dd2eebc63
laura-bvc <156967330+laura-bvc@users.noreply.github.com>
2024-04-07 16:27:25 -0600

    V1.1 minor typo fix

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@
 	
 		
 	// json from https://gist.github.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c
-	const wmo = "https://gist.githubusercontent.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c/raw/76b0cb0ef0bfd8a2ec988aa54e30ecd1b483495d/descriptions.json";
-	// const wmo = "./descriptions.json";
+	//const wmo = "https://gist.githubusercontent.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c/raw/76b0cb0ef0bfd8a2ec988aa54e30ecd1b483495d/descriptions.json";
+	const wmo = "./descriptions.json";
 	var wmo_json;
 	
 	fetch(wmo)
